c6dd5ca5b67fb2603981c9c5ad2c389de021801f
boxcatdev <boxcatdev@gmail.com>
2023-10-24 19:51:52 -0500

    added card flip feature and moved files around
    going to add the "upgrade" feature next so you can combine lower level collectables to get higher level ones

---
+++ Assets/_Project/Scripts/Gacha/CardObject.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardObject : MonoBehaviour
+{
+    [Header("Transforms")]
+    [SerializeField] private GameObject cardFront;
+    [SerializeField] private GameObject cardBack;
+
+    [Header("Attributes")]
+    [SerializeField] private bool flippable;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        animator.enabled = false;
+
+        //enable cursor
+        var input = FindObjectOfType<StarterAssets.StarterAssetsInputs>();
+        input.SetCursorState(false);
+    }
+    private void Start()
+    {
+        ChangeToCardBack();
+    }
+
+    #region Animation Functions
+    private void DisableAnimator()
+    {
+        animator.enabled = false;
+    }
+    private void FlipCard()
+    {
+        if (!flippable) return;
+
+        animator.enabled = true;
+        animator.Play("CardFlip");
+    }
+    private void ChangeToCardBack()
+    {
+        if (cardFront != null && cardBack != null)
+        {
+            cardFront.SetActive(false);
+            cardBack.SetActive(true);
+        }
+    }
+    private void ChangeToCardFront()
+    {
+        if (cardFront != null && cardBack != null)
+        {
+            cardFront.SetActive(true);
+            cardBack.SetActive(false);
+        }
+    }
+    #endregion
+}
