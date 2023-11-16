73fb02a3a0019073629e536c4c03ff12b302af79
boxcatdev <boxcatdev@gmail.com>
2023-11-16 14:33:11 -0600

    added upgrade to Card Objects
    also added new UI assets

--- Assets/_Project/Scripts/Gacha/CardObject.cs
+++ Assets/_Project/Scripts/Gacha/CardObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class CardObject : MonoBehaviour
 {
@@ -16,9 +17,32 @@ public class CardObject : MonoBehaviour
     [Header("Card Properties")]
     [SerializeField] private Rarity rarity;
 
+    [Header("Upgrade Properties")]
+    //[SerializeField] private UpgradeCost upgradeCost;
+    [SerializeField] private Rarity upgradeRarity;
+    [SerializeField, Range(1, 10)] private int upgradeCost;
+    [SerializeField] private GameObject upgradeButton;
+
+    [Header("Visibility Settings")]
+    [SerializeField] private Image CardGemIcon;
+    [Space]
+    [SerializeField] private TextMeshProUGUI textUpgradeCost;
+    [SerializeField] private Image imgUpgradeGem;
+    [Space]
+    [SerializeField] private List<Color> gemColors = new List<Color>();
+
+    //references
     private Animator animator;
 
+    //private variables
     private bool isBack;
+
+    [Serializable]
+    public struct UpgradeCost
+    {
+        Rarity rarity;
+        int amount;
+    }
 
     private void Awake()
     {
@@ -33,6 +57,28 @@ public class CardObject : MonoBehaviour
     {
         ChangeToCardBack();
     }
+
+    #region Gacha Stuff
+    public void UpgradeCard()
+    {
+        //check if player has enough to upgrade
+
+        //do vfx for changing card value
+        //change card value
+        //do vfx to disable upgrade button
+        //disable upgrade button
+
+        bool hasCost = true;
+        if(hasCost)
+        {
+            //change card value
+            rarity = upgradeRarity;
+
+            //disable upgrade button
+            if(upgradeButton != null) upgradeButton.SetActive(false);
+        }
+    }
+    #endregion
 
     #region Animation Functions
     private void DisableAnimator()
@@ -63,6 +109,26 @@ public class CardObject : MonoBehaviour
             cardBack.SetActive(false);
             isBack = false;
         }
+    }
+    #endregion
+
+    #region Visibility Functions
+    private void Update()
+    {
+        //change Gem Icon color
+        if (rarity == Rarity.Common && CardGemIcon.color != gemColors[0]) CardGemIcon.color = gemColors[0];
+        if (rarity == Rarity.Uncommon && CardGemIcon.color != gemColors[1]) CardGemIcon.color = gemColors[1];
+        if (rarity == Rarity.Rare && CardGemIcon.color != gemColors[2]) CardGemIcon.color = gemColors[2];
+        if (rarity == Rarity.UltraRare && CardGemIcon.color != gemColors[3]) CardGemIcon.color = gemColors[3];
+
+        //change Gem Cost color
+        if (upgradeRarity == Rarity.Common && imgUpgradeGem.color != gemColors[0]) imgUpgradeGem.color = gemColors[0];
+        if (upgradeRarity == Rarity.Uncommon && imgUpgradeGem.color != gemColors[1]) imgUpgradeGem.color = gemColors[1];
+        if (upgradeRarity == Rarity.Rare && imgUpgradeGem.color != gemColors[2]) imgUpgradeGem.color = gemColors[2];
+        if (upgradeRarity == Rarity.UltraRare && imgUpgradeGem.color != gemColors[3]) imgUpgradeGem.color = gemColors[3];
+
+        //change Gem Cost amount
+        if(textUpgradeCost.text != upgradeCost.ToString()) textUpgradeCost.text = upgradeCost.ToString();
     }
     #endregion
 
